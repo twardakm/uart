1828827f06f8c5ecd5d2e08d91e4a6075c936638
twardakm <twardakm@gmail.com>
2013-11-10 14:46:32 +0100

    Działa UART w AtMedze

--- main.c
+++ main.c
@@ -4,17 +4,26 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "uart_atmega.h"
 
 int main(void)
 {
     DDRB |= 0x03;
     PORTB |= 0x03;
 
+    USART_Init(MYUBRR);
+    unsigned char temp;
     while(1)
     {
+        USART_Transmit(1);
         PORTB ^= 0x01;
         _delay_ms(1000);
-        PORTB ^= 0x02;
+        if (RXC)
+        {
+            temp = USART_Receive();
+            if (temp == 1)
+                PORTB ^= 0x02;
+        }
         _delay_ms(1000);
     }
 
