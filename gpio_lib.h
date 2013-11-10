261509951ad79b6e1c2ca4de050fc5089a542e06
twardakm <twardakm@gmail.com>
2013-11-10 17:03:26 +0100

    Zdefiniowane porty

--- gpio_lib.h
+++ gpio_lib.h
@@ -1,6 +1,54 @@
 #ifndef _GPIO_LIB_H_
 #define _GPIO_LIB_H_
 
+/*Definiuję porty z pyA13.c
+* ------------------- */
+#define PIN4    SUNXI_GPB(0)
+#define PIN5    SUNXI_GPB(1)
+#define PIN6    SUNXI_GPB(2)
+#define PIN7    SUNXI_GPB(3)
+#define PIN8    SUNXI_GPB(4)
+#define PIN9    SUNXI_GPB(10)
+#define PIN10   SUNXI_GPB(15)
+#define PIN11   SUNXI_GPB(16)
+
+#define PIN12   SUNXI_GPC(0)
+#define PIN13   SUNXI_GPC(1)
+#define PIN14   SUNXI_GPC(2)
+#define PIN15   SUNXI_GPC(3)
+#define PIN16   SUNXI_GPC(4)
+#define PIN17   SUNXI_GPC(5)
+#define PIN18   SUNXI_GPC(6)
+#define PIN19   SUNXI_GPC(7)
+#define PIN20   SUNXI_GPC(8)
+#define PIN21   SUNXI_GPC(9)
+#define PIN22   SUNXI_GPC(10)
+#define PIN23   SUNXI_GPC(11)
+#define PIN24   SUNXI_GPC(12)
+#define PIN25   SUNXI_GPC(13)
+#define PIN26   SUNXI_GPC(14)
+#define PIN27   SUNXI_GPC(15)
+#define PIN28   SUNXI_GPC(19)
+
+#define PIN29   SUNXI_GPE(4)
+#define PIN30   SUNXI_GPE(5)
+#define PIN31   SUNXI_GPE(6)
+#define PIN32   SUNXI_GPE(7)
+#define PIN33   SUNXI_GPE(8)
+#define PIN34   SUNXI_GPE(9)
+#define PIN35   SUNXI_GPE(10)
+#define PIN36   SUNXI_GPE(11)
+
+#define PIN37   SUNXI_GPG(9)
+#define PIN38   SUNXI_GPG(10)
+#define PIN39   SUNXI_GPG(11)
+
+#define MISO    SUNXI_GPE(3)
+#define MOSI    SUNXI_GPE(2)
+#define SCK     SUNXI_GPE(1)
+#define CS      SUNXI_GPE(0)
+/*--------------------------*/
+
 
 #define SW_PORTC_IO_BASE 0x01c20800
 
