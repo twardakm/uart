bf478c8743dbc5265a1160621bfcda30d776c986
twardakm <twardakm@gmail.com>
2013-11-10 14:04:43 +0100

    Mrugają dido, naprawione ustawienia kompilacji w CodeBlocks

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 /*
  */
+#define F_CPU 1000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
-
-    DDRB = 0xff;
-    PORTB = 0xff;
+    DDRB |= 0x03;
+    PORTB |= 0x03;
 
     while(1)
-    ;
+    {
+        PORTB ^= 0x01;
+        _delay_ms(1000);
+        PORTB ^= 0x02;
+        _delay_ms(1000);
+    }
 
     return 0;
 }
