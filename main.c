ee985586ae11c38411e22a0eae6b67871bb84c98
twardakm <twardakm@gmail.com>
2013-11-17 13:31:01 +0100

    naprawione opcje projektu

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 /*
  */
-#define F_CPU 1000000UL
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart_atmega.h"
@@ -9,6 +7,9 @@
 int main(void)
 {
     DDRB |= 0x03;
+    PORTB = 0;
+    PORTB |= 0x03;
+    _delay_ms(2000);
     PORTB = 0;
 
     USART_Init(MYUBRR);
